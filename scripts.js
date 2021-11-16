e89d75ccf0731b024535bec9fe2496b5cf12b36f
unknown <kdagudelo70@misena.edu.co>
2021-11-16 17:25:20 +0100

    Terminada la funcion create card

--- scripts.js
+++ scripts.js
@@ -1,14 +1,25 @@
 const URL= 'https://rickandmortyapi.com/api/character/';
-const main=document.querySelector(main)
+const main=document.querySelector("#main")
+const characterOptions=document.querySelector("#characterOptions")
+const allCharacters=document.querySelector('allCharacters')
 
 function validateUrl(){
     fetch(URL)
     .then(response=>response.json())
-    .then(data=>)
+    .then(data=>{
+        data.results.map(items=> {
+            const option=document.createElement('option')
+            option.textContent=items.name
+        });
+    })
+    .catch(error => console.log(error))
+
 }
 
+validateUrl()
 
-function  createCard();{
+
+function createCard(data){
     const bigCard=document.createElement('div');
     bigCard.classList.add('bigCard')
     const cardBackground=document.createElement('div');
@@ -18,10 +29,15 @@ function  createCard();{
     const nameCharacter=document.createElement('p')
     nameCharacter.classList.add('nameCharacter')
 
+    imgDiv.src=data.image;
+    nameCharacter.textContent=data.name
+
     bigCard.appendChild(cardBackground);
     bigCard.appendChild(imgDiv);
     bigCard.appendChild(nameCharacter);
     main.appendChild(bigCard);
+
+    
 }
 
-createCard();
+console.log('hola')
